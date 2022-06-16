e6a3d99a22931bc46fd8144da337fc23d3db9cff
Manshal <manshalkhatri51@gmail.com>
2022-06-16 14:23:36 +0530

    Added Deep Link to Add Repositories to App

--- app/src/main/java/com/manshal_khatri/githubbrowser/activity/HomeActivity.kt
+++ app/src/main/java/com/manshal_khatri/githubbrowser/activity/HomeActivity.kt
@@ -1,22 +1,35 @@
 package com.manshal_khatri.githubbrowser.activity
 
 import android.content.Intent
+import android.database.sqlite.SQLiteConstraintException
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.manshal_khatri.githubbrowser.R
 import com.manshal_khatri.githubbrowser.adapter.GitRepositoryAdapter
 import com.manshal_khatri.githubbrowser.databinding.ActivityHomeBinding
+import com.manshal_khatri.githubbrowser.model.GitRepository
 import com.manshal_khatri.githubbrowser.util.BaseActivity
+import com.manshal_khatri.githubbrowser.util.Constants
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeActivity : BaseActivity() {
     lateinit var toolbar : Toolbar
@@ -53,9 +66,61 @@ class HomeActivity : BaseActivity() {
                 rvGitRepositories.adapter = GitRepositoryAdapter(it)
             }
         })
-
+        val data : Uri? = intent?.data
+        if (data!=null) {
+            addExternalRepo(data)
+        }
     }
 
+
+    fun addExternalRepo(data : Uri){
+
+            val params : List<String> = data.pathSegments
+            if(params.size==2){
+                val repo =params[params.size-1]
+                val owner = params[params.size-2]
+
+                val queue = Volley.newRequestQueue(this)
+                val reqGitRepo = object :
+                    JsonObjectRequest(
+                        Method.GET,
+                        Constants.API_GITHUB+"$owner/$repo",
+                        null,
+                        Response.Listener {
+                            viewModel.setUpDataBase(this)
+                            val gitRepo = GitRepository(
+                                it.getInt("id").toString(),
+                                owner,
+                                repo,
+                                it.getString("description"),
+                                it.getString("html_url"))
+                            CoroutineScope(Dispatchers.IO).launch{
+                              try {
+                                  viewModel.addRepo(gitRepo)
+                                  withContext(Main){
+                                      Toast.makeText(this@HomeActivity, "Added", Toast.LENGTH_SHORT).show()
+                                  }
+                              }catch (e : Exception){
+                                  println(e)
+                                  when(e){
+                                      is SQLiteConstraintException -> withContext(Main){
+                                          Toast.makeText(
+                                              this@HomeActivity,
+                                              "You Already have this Repository",
+                                              Toast.LENGTH_SHORT
+                                          ).show()
+                                      }
+                                  }
+                              }
+                            }
+                        }, Response.ErrorListener {
+                            Toast.makeText(this, "Repository Not Found", Toast.LENGTH_SHORT).show()
+                        }){}
+                queue.add(reqGitRepo)
+            }else{
+                Toast.makeText(this, "Invalid Repository", Toast.LENGTH_SHORT).show()
+            }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_home_activity, menu)
--- app/src/main/java/com/manshal_khatri/githubbrowser/viewmodel/AddRepoViewModel.kt
+++ app/src/main/java/com/manshal_khatri/githubbrowser/viewmodel/AddRepoViewModel.kt
@@ -1,6 +1,7 @@
 package com.manshal_khatri.githubbrowser.viewmodel
 
 import android.content.Context
+import android.database.sqlite.SQLiteConstraintException
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.android.volley.Response
@@ -8,10 +9,7 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.manshal_khatri.githubbrowser.model.GitRepository
 import com.manshal_khatri.githubbrowser.util.Constants
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class AddRepoViewModel : HomeViewModel() {
 
@@ -32,9 +30,24 @@ class AddRepoViewModel : HomeViewModel() {
                         it.getString("description"),
                         it.getString("html_url"))
                     CoroutineScope(Dispatchers.IO).launch{
-                        addRepo(gitRepo)
+                        try {
+                            addRepo(gitRepo)
+                            withContext(Dispatchers.Main){
+                                Toast.makeText(context, "Added", Toast.LENGTH_SHORT).show()
+                            }
+                        } catch (e: Exception) {
+                            println(e)
+                            when(e){
+                                is SQLiteConstraintException -> withContext(Dispatchers.Main){
+                                    Toast.makeText(
+                                        context,
+                                        "You Already have this Repository",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        }
                     }
-                    Toast.makeText(context, "Added", Toast.LENGTH_SHORT).show()
                 }, Response.ErrorListener {
                     Toast.makeText(context, "Repository Not Found", Toast.LENGTH_SHORT).show()
                 }){}
